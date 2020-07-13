ec809067e0b54d464398781de4a9e4c09230b149
Trong Hai <truongtronghai@gmail.com>
2020-07-14 06:18:44 +0700

    hoan tat ham shorteningUrlV2

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -21,15 +21,16 @@ class UrlController extends Controller
     }
     
     public function store(Request $req){
-        //$this->setLocale();
-
         $url = new Url();
-        $url = $url->where('url',$req->input('originalUrl'))->first();
+        $url = $url->where([
+            ['url',$req->input('originalUrl')],
+            //['expired_at','<=',date('Y-m-d H:i:s')]
+            ])->first();
 
         if(isset($url)){
             $resultUrl = $url->shortened;
         }else{
-            $resultUrl = $this->shorteningUrl();
+            $resultUrl = $this->shorteningUrlV2();
             // save vao db
             try{
                 $url = new Url([
@@ -51,34 +52,32 @@ class UrlController extends Controller
         // update vao db
         try{
             $url = new Url();
-            $url = $url->where('shortened',$short)->first();
+            $url = $url->where([
+                    ['shortened',$short],
+                    ['expired_at','>=',date('Y-m-d H:i:s')] // cac url chua het han
+                ])->first();
+                
             $url->count = $url->count + 1;
-            $url->save();
+            $url->save(); // neu dieu kien truy xuat tren khong dung ( khong co URL thoa dieu kien ) thi tai day se throw ra error
             return redirect()->away($url->url);
         }catch(\Exception $e){
             Log::debug('UrlController::run('.$short.') : '.$e->getMessage());
-            session()->flash('sessionNotification','
-            Sorry! Your short URL does not exist. You need to check it or create new one with this app.
-             | 
-            Rất tiếc địa chỉ rút gọn của bạn cung cấp không tồn tại. Bạn nên kiểm tra lại với người cung cấp hoặc tạo cái mới với ứng dụng này.
-            ');
+            session()->flash('sessionNotification',__('messages.textShortUrlErrorMessage'));
             return redirect('/');
         }
     }
     /**
      * Thuat toan dung de phat sinh chuoi rut gon cho URL.
-     * - Phat sinh chuoi 3 ky tu ngau nhien ( goi la S1 )
      * - Lay ID lon nhat + 1 ( ID cho record moi ke tiep )
      * - Chuyen ID nay thanh chuoi
-     * - map() | transform() chuoi ID nay thanh chuoi khac ( goi la S2 )
-     * - Tao ket qua la S = S1 + S2
+     * - map() | transform() chuoi ID nay thanh chuoi
      */
     protected function shorteningUrl(){
-        $s1 = str_split(Str::random(3));
-        $s2 = URL::max('id')+1; // lay ID cao nhat hien tai va tang len 1
-        // chuyen S2 thanh collection de su dung ham transform() cua laravel
-        $s2 = collect(str_split(strval($s2)));
-        $s2->transform(function($item,$key){
+        $s = URL::max('id')+1; // lay ID cao nhat hien tai va tang len 1
+        // chuyen S thanh collection de su dung ham transform() cua laravel
+        $s = collect(str_split(strval($s)));
+
+        $s->transform(function($item,$key){
             $transformCharacterCollection = [
                 '0' => 'a',
                 '1' => 'x',
@@ -94,19 +93,46 @@ class UrlController extends Controller
 
             return $transformCharacterCollection[$item];
         });
-        $s2 = $s2->all(); // chuyen S2 tu collection tro lai thanh array
         
-        return implode(array_merge($s1,$s2));
+        $s = $s->all(); // chuyen S2 tu collection tro lai thanh array
+        
+        return implode($s);
     } 
     /**
+     * Chua nghi ra thuat toan de su dung tat ca 58 ky tu de xay dung chuoi rut gon
      */
-    protected function shorteningUrlV2(string $longUrl){
+    protected function shorteningUrlV2(){
+        /*
         $arr_letters = ['', // dem ky tu nay de cac ky tu phia sau bat dau o vi tri 1
             '0','1','2','3','4','5','6','7','8','0',
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
             'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
         ]; // 58 ky tu dung de tao chuoi ngau nhien
-        $nLeters = 58; // tong so ky tu bao gom 10 so va 48 chu hoa va thuong 
+        */
+        $nLeters = 58; // tong so ky tu bao gom 10 so va 48 chu hoa va thuong
+        $id = URL::max('id')+1; // lay ID cao nhat hien tai va tang len 1
+        
+        /**
+         *  tim so ky tu cua chuoi shorten dua vao gia tri cua id
+         *  neu chuoi 1 ky tu se co 58 truong hop
+         *  neu chuoi 2 ky tu se co 58 * 58 truong hop
+         *  neu chuoi 3 ky tu se co 58 * 58 * 58 truong hop
+         *  neu chuoi n ky tu se co 58^n truong hop 
+         */
+        $n=0;
+        do{
+            $n++;
+        }while($id>pow($nLeters,$n));
+        // tao chuoi s la random voi so ky tu la $n
+        
+        $s = Str::random($n);
+        // kiem tra xem chuoi shorten co ton tai hay chua
+        $shortStr = new Url();
+        while($shortStr->where('shortened',$s)->exists()){ // neu chuoi nay chua co trong DB
+            $s = Str::random($n); // tao lai chuoi khac va tiep tuc kiem tra
+        }
+        // neu ra khoi vong while nghia la chuoi $s chua co trong DB. Co the dung tao chuoi moi duoc
+        return $s;
     }
 
 }
